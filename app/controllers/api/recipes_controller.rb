59f59dc961680189a6324223c10c7af7092d5180
robocop019 <chrisbowen@comcast.net>
2020-02-25 14:15:42 -0600

    added update and destroy restful routes

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -21,4 +21,25 @@ class Api::RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     render 'show.json.jbuilder'
   end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+
+    @recipe.save
+
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    render json: {message: 'Successfully destroyed recipe.'}
+  end
 end
