3a0ffcb5dbc02ecad25dc586e53ef9568da5bc90
robocop019 <chrisbowen@comcast.net>
2020-02-24 22:36:06 -0600

    added create restful route

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -4,6 +4,19 @@ class Api::RecipesController < ApplicationController
     render 'index.json.jbuilder'
   end
 
+  def create
+    @recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        prep_time: params[:prep_time],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions],
+                        image_url: params[:image_url]
+                        )
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     render 'show.json.jbuilder'
