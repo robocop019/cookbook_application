5b6af956372d1ca8f5eae80b13fdb012833ba10b
robocop019 <chrisbowen@comcast.net>
2020-02-24 22:03:12 -0600

    added restful routes for index and show

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,6 +1,11 @@
 class Api::RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.last
-    render 'one_recipe_view.json.jbuilder'
+  def index
+    @recipes = Recipe.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jbuilder'
   end
 end
--- app/views/api/recipes/show.json.jbuilder
+++ app/views/api/recipes/show.json.jbuilder
@@ -1,6 +1,7 @@
 json.id @recipe.id
 json.title @recipe.title
 json.chef @recipe.chef
+json.prep_time @recipe.prep_time
 json.ingredients @recipe.ingredients
 json.directions @recipe.directions
-json.prep_time @recipe.prep_time
+json.image_url @recipe.image_url
